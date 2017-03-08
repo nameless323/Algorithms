bf4cf65a70d15e71bb573c91dbe79a06c57c9878
Alex Vorontsov <nameless323@gmail.com>
2017-03-08 18:42:11 +0300

    Initial commit

---
+++ Algorithms.cpp
@@ -0,0 +1,15 @@
+//
+// Copyright (C) Alexandr Vorontsov 2016.
+// Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 5;
+    std::cout << x;
+    getchar();
+    return 0;
+}
+
