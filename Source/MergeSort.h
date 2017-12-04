2b6188d1406cfcfacda40ef5910530a12c07f680
Alex Vorontsov <nameless323@gmail.com>
2017-12-04 23:43:20 +0300

    Non templated merge sort.

--- Source/MergeSort.h
+++ Source/MergeSort.h
@@ -4,11 +4,11 @@
 
 namespace Algorithms
 {
-void Merge(std::vector<int> v, int start, int median, int end);
+void Merge(std::vector<int>& v, int start, int median, int end);
 
 void MergeSort(std::vector<int>& v, int start, int end)
 {
-    if (start >= end)
+    if (end - start == 1)
         return;
     int median = (start + end) / 2;
     MergeSort(v, start, median);
@@ -16,12 +16,21 @@ void MergeSort(std::vector<int>& v, int start, int end)
     Merge(v, start, median, end);
 }
 
-void Merge(std::vector<int> v, int start, int median, int end)
+void Merge(std::vector<int>& v, int start, int median, int end)
 {
     std::vector<int> left(v.begin() + start, v.begin() + median);
     std::vector<int> right(v.begin() + median, v.end());
     int marker = std::numeric_limits<int>::max();
     left.push_back(marker);
     right.push_back(marker);
+
+    int l = 0, r = 0;
+    for (int m = start; m < end; ++m)
+    {
+        if (left[l] < right[r])
+            v[m] = left[l++];
+        else
+            v[m] = right[r++];
+    }
 }
 }
